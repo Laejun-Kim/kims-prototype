33fe669177b2211cf6fa74bb9c017a83fbfa56f2
Laejun Kim <49670820+Laejun-Kim@users.noreply.github.com>
2024-03-24 14:33:10 +0900

    초기 페이지 설정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
-import Image from 'next/image';
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <p>안녕?!!</p>
-      <p>다디단 밤양갱~</p>
+    <main>
+      <h1 className="text-3xl font-bold">
+        달력 라이브러리와 팀빌딩 ui 테스트 프로토타입
+      </h1>
+      <Link href="/calendar">캘린더</Link>
+      <br />
+
+      <Link href="/teambuilding">팀빌딩</Link>
     </main>
   );
 }
---
+++ src/app/teambuilding/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TeamBulidingPage = () => {
+  return <div>TeamBulidingPage</div>;
+};
+
+export default TeamBulidingPage;
