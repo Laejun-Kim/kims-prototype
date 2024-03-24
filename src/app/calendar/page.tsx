6722d865d08d34ff3d170a2c9f039ed494bdd503
Laejun Kim <49670820+Laejun-Kim@users.noreply.github.com>
2024-03-24 19:53:25 +0900

    DnD

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -1,35 +1,32 @@
-'use client';
-import React, { useState } from 'react';
-import Calendar from 'react-calendar';
-import moment from 'moment';
-import Swal from 'sweetalert2';
+"use client";
+import React, { useState } from "react";
+import Calendar from "react-calendar";
+import moment from "moment";
+import Swal from "sweetalert2";
 
-// type ScheduleForm = {
-//   timestamp: string;
-//   content: string;
-// };
 type scheduletype = { content: string; starttime: string; endtime: string };
 
 const CalendarPage = () => {
   const [date, onChange] = useState(new Date());
   const [schedules, setSchedule] = useState<scheduletype[]>([]);
+  //   const [showSchedule, setShowSchedule] = useState(false);
   console.log(date);
 
   let starttimeInput: HTMLInputElement;
   let endtimeInput: HTMLInputElement;
   let contentInput: HTMLInputElement;
 
-  const dayList = ['2024-03-26', '2024-03-27'];
+  const dayList = ["2024-03-26", "2024-03-27"];
 
   const addContent = ({ date }: any) => {
     // 해당 날짜(하루)에 추가할 컨텐츠의 배열
     const contents = [];
 
     // date(각 날짜)가  리스트의 날짜와 일치하면 해당 컨텐츠(이모티콘) 추가
-    if (dayList.find((day) => day === moment(date).format('YYYY-MM-DD'))) {
+    if (dayList.find((day) => day === moment(date).format("YYYY-MM-DD"))) {
       contents.push(
         <>
-          <div className='w-[20px] h-[20px] bg-slate-300 rounded-full'>*</div>
+          <div className="w-[20px] h-[20px] bg-slate-300 rounded-full">*</div>
         </>
       );
     }
@@ -41,64 +38,82 @@ const CalendarPage = () => {
       <div>캘린더를 화면에 뿌려보자</div>
       <Calendar
         value={date}
-        formatDay={(locale, date) => moment(date).format('D')}
+        formatDay={(locale, date) => moment(date).format("D")}
         tileContent={addContent}
         onChange={onChange}
-        locale='ko'
+        locale="ko"
         showNeighboringMonth={false}
         onClickDay={(date) => {
           Swal.fire({
-            title: moment(date).format('YYYY-MM-DD'),
-            html: `
-            <input type="time" id="starttime" class="swal2-input" placeholder="Password">
-            <input type="time" id="endtime" class="swal2-input" placeholder="Password">
-                  <input type="text" id="content" class="swal2-input" placeholder="content">
-                `,
-            confirmButtonText: '일정 저장',
+            title: "sample",
+            showDenyButton: true,
             showCancelButton: true,
-            cancelButtonText: '취소',
-            focusConfirm: false,
-            didOpen: () => {
-              const popup = Swal.getPopup()!;
-              contentInput = popup.querySelector(
-                '#content'
-              ) as HTMLInputElement;
-              starttimeInput = popup.querySelector(
-                '#starttime'
-              ) as HTMLInputElement;
-              endtimeInput = popup.querySelector(
-                '#endtime'
-              ) as HTMLInputElement;
-
-              //   contentInput.onkeyup = (event) =>
-              //     event.key === "Enter" && Swal.clickConfirm();
-              //   timeInput.onkeyup = (event) =>
-              //     event.key === "Enter" && Swal.clickConfirm();
-            },
-            preConfirm: () => {
-              const content = contentInput.value;
-              const starttime = starttimeInput.value;
-              const endtime = endtimeInput.value;
-
-              return { content, starttime, endtime };
-            },
+            confirmButtonText: "일정 확인하기",
+            denyButtonText: `일정 추가하기`,
           }).then((result) => {
-            console.log('리설트 벨류', result.value);
-            setSchedule((prev) => [...prev, result.value]);
-            console.log('스케줄', schedules);
+            /* Read more about isConfirmed, isDenied below */
+            if (result.isConfirmed) {
+              Swal.fire("Saved!", "", "success");
+            } else if (result.isDenied) {
+              Swal.fire({
+                title: moment(date).format("YYYY-MM-DD"),
+                html: `    <label for="starttime">시작시간</label>
+                        <input type="datetime-local" id="starttime" class="swal2-input" placeholder="Password">
+                        <br/>
+                        <label for="endtime">끝나는 시간</label>
+                        <input type="datetime-local" id="endtime" class="swal2-input" placeholder="Password">
+                              <input type="text" id="content" class="swal2-input" placeholder="content">
+                            `,
+                confirmButtonText: "일정 저장",
+                showCancelButton: true,
+                cancelButtonText: "취소",
+                focusConfirm: false,
+                didOpen: () => {
+                  const popup = Swal.getPopup()!;
+                  contentInput = popup.querySelector(
+                    "#content"
+                  ) as HTMLInputElement;
+                  starttimeInput = popup.querySelector(
+                    "#starttime"
+                  ) as HTMLInputElement;
+                  endtimeInput = popup.querySelector(
+                    "#endtime"
+                  ) as HTMLInputElement;
+
+                  //   contentInput.onkeyup = (event) =>
+                  //     event.key === "Enter" && Swal.clickConfirm();
+                  //   timeInput.onkeyup = (event) =>
+                  //     event.key === "Enter" && Swal.clickConfirm();
+                },
+                preConfirm: () => {
+                  const content = contentInput.value;
+                  const starttime = starttimeInput.value;
+                  const endtime = endtimeInput.value;
+
+                  return { content, starttime, endtime };
+                },
+              }).then((result) => {
+                console.log("리설트 벨류", result.value);
+                setSchedule((prev) => [...prev, result.value]);
+                console.log("스케줄", schedules);
+              });
+            }
           });
         }}
       />
-      <div>{moment(date).format('Y년-M월-D일')}</div>
+      <div>{moment(date).format("Y년-M월-D일")}</div>
       <div>
         {schedules?.map((schedule) => {
           return (
             <>
               {schedule ? (
                 <>
-                  <p>시작시간:{schedule.starttime}</p>
-                  <br />
-                  <p>끝나는 시간:{schedule.endtime}</p>
+                  <h1>
+                    일정 날짜:{moment(schedule.starttime).format("MM-DD")}
+                  </h1>
+                  <p>시작시간:{moment(schedule.starttime).format("HH:MM")}</p>
+
+                  <p>끝나는 시간:{moment(schedule.endtime).format("HH:MM")}</p>
                   <br />
                   <p>{schedule.content}</p>
                 </>
--- src/app/teambuilding/page.tsx
+++ src/app/teambuilding/page.tsx
@@ -1,7 +1,114 @@
-import React from "react";
+"use client";
+// import { TItems, TItemStatus } from "@/pages/todo";
+// import { $ } from "@/utils";
+import { useEffect, useState } from "react";
+import {
+  DragDropContext,
+  Draggable,
+  Droppable,
+  DropResult,
+} from "react-beautiful-dnd";
 
-const TeamBulidingPage = () => {
-  return <div>TeamBulidingPage</div>;
+type TItemStatus = "todo" | "doing";
+
+export type TItem = {
+  id: string;
+  status: TItemStatus;
+  title: string;
 };
 
-export default TeamBulidingPage;
+export type TItems = {
+  [key in TItemStatus]: TItem[];
+};
+
+export default function TodoLibraryExample({
+  items,
+  setItems,
+}: {
+  items: TItems;
+  setItems: (items: TItems) => void;
+}) {
+  const onDragEnd = ({ source, destination }: DropResult) => {
+    if (!destination) return;
+
+    const scourceKey = source.droppableId as TItemStatus;
+    const destinationKey = destination.droppableId as TItemStatus;
+
+    const _items = JSON.parse(JSON.stringify(items)) as typeof items;
+    const [targetItem] = _items[scourceKey].splice(source.index, 1);
+    _items[destinationKey].splice(destination.index, 0, targetItem);
+    setItems(_items);
+  };
+
+  // --- requestAnimationFrame 초기화
+  const [enabled, setEnabled] = useState(false);
+
+  useEffect(() => {
+    const animation = requestAnimationFrame(() => setEnabled(true));
+
+    return () => {
+      cancelAnimationFrame(animation);
+      setEnabled(false);
+    };
+  }, []);
+
+  const dummyArr = [
+    { id: "1", status: "todo", title: "김미희" },
+    { id: "2", status: "todo", title: "김래준" },
+    { id: "3", status: "todo", title: "김대영" },
+    { id: "4", status: "todo", title: "김은채" },
+  ];
+
+  if (!enabled) {
+    return null;
+  }
+  // --- requestAnimationFrame 초기화 END
+
+  return (
+    <div className="p-4">
+      <div className="mb-2">
+        <h1 className="text-3xl font-bold">react-beautiful-dnd</h1>
+        <span>with react library</span>
+      </div>
+
+      <div className="mt-4 flex">
+        <DragDropContext onDragEnd={onDragEnd}>
+          <div className="grid flex-1 select-none grid-cols-2 gap-4 rounded-lg">
+            {dummyArr.map((key) => (
+              <Droppable key={key} droppableId={key}>
+                {(provided, snapshot) => (
+                  <div ref={provided.innerRef} {...provided.droppableProps}>
+                    <span className="text-xs font-semibold">
+                      {/* {key.toLocaleUpperCase()} */}
+                    </span>
+                    {dummyArr.map((item, index) => (
+                      <Draggable
+                        key={item.id}
+                        draggableId={item.id}
+                        index={index}
+                      >
+                        {(provided, snapshot) => (
+                          <div
+                            ref={provided.innerRef}
+                            {...provided.draggableProps}
+                            {...provided.dragHandleProps}
+                          >
+                            <h5 className="font-semibold">{item.title}</h5>
+                            <span className="text-sm text-gray-500">
+                              이거 인간적으로 너무 어렵다
+                            </span>
+                          </div>
+                        )}
+                      </Draggable>
+                    ))}
+                    {provided.placeholder}
+                  </div>
+                )}
+              </Droppable>
+            ))}
+          </div>
+        </DragDropContext>
+      </div>
+    </div>
+  );
+}
