11cbb86a98dd36bb69d09f7f866abee8f57bdb54
mi-hee-k <90510192+mi-hee-k@users.noreply.github.com>
2024-03-24 14:26:26 +0900

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <p>안녕?!!</p>
+      <p>다디단 밤양갱~</p>
     </main>
   );
 }
