181669f2e0e918f5684bda6422aefffc76dc5ca7
mi-hee-k <90510192+mi-hee-k@users.noreply.github.com>
2024-03-27 21:48:20 +0900

    조 옮김 성공

--- src/app/teambuilding/page.tsx
+++ src/app/teambuilding/page.tsx
@@ -1,14 +1,14 @@
-"use client";
+'use client';
 
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 import {
   DragDropContext,
   Draggable,
   Droppable,
   DropResult,
-} from "react-beautiful-dnd";
+} from 'react-beautiful-dnd';
 
-type TItemStatus = "todo" | "doing";
+type TItemStatus = 'todo' | 'doing';
 
 export type TItem = {
   id: string;
@@ -22,16 +22,17 @@ export type TItems = {
 
 export default function TodoLibraryExample() {
   const dummyArr = [
-    { id: "01", team: "1", title: "김미희" },
-    { id: "02", team: "1", title: "김래준" },
-    { id: "03", team: "1", title: "김대영" },
-    { id: "04", team: "2", title: "김은채" },
+    { id: '01', team: '1', title: '김미희' },
+    { id: '02', team: '1', title: '김래준' },
+    { id: '03', team: '1', title: '김대영' },
+    { id: '04', team: '2', title: '김은채' },
   ];
 
   const [people, setPeople] = useState(dummyArr);
 
   const handleDragDrop = (results: DropResult) => {
-    console.log("드래그앤드롭이 발생함!!", results);
+    console.log('드래그앤드롭이 발생함!!', results);
+
     //result 의 source 와 destination 에 출발지점과 도착지점이 기록된다
     const { source, destination, type } = results;
     if (!destination) return; //엉뚱한 곳에 내려놓으면 아무것도 안하겠단 거임
@@ -41,7 +42,7 @@ export default function TodoLibraryExample() {
     )
       return;
     //집어 올렸다가 같은 곳에 내려놔도 아무것도 안하겠단 거고
-    if (type === "group") {
+    if (source.droppableId === destination.droppableId) {
       const reorderedPeople = [...people];
       const sourceIndex = source.index;
       const destinationIndex = destination.index;
@@ -50,13 +51,27 @@ export default function TodoLibraryExample() {
 
       return setPeople(reorderedPeople);
     }
-    const teamSourceIndex = people.findIndex(
-      (person) => person.id === source.droppableId
-    );
-    const teamDestinationIndex = people.findIndex(
-      (person) => person.id === destination.droppableId
-    );
-    const newSourceItems = [...people[teamSourceIndex].items];
+
+    if (source.droppableId !== destination.droppableId) {
+      const reorderedPeople = [...people];
+      console.log('잡은 사람 아이디:', results.draggableId);
+      reorderedPeople.filter((item) => {
+        if (item.id === results.draggableId) {
+          item.team = destination.droppableId;
+        }
+        return item;
+      });
+      console.log(people);
+      return setPeople(reorderedPeople);
+    }
+
+    // const teamSourceIndex = people.findIndex(
+    //   (person) => person.id === source.droppableId
+    //
+    // const teamDestinationIndex = people.findIndex(
+    //   (person) => person.id === destination.droppableId
+    // );
+    // const newSourceItems = [...people[teamSourceIndex].items];
   };
 
   // --- requestAnimationFrame 초기화
@@ -77,62 +92,91 @@ export default function TodoLibraryExample() {
   // --- requestAnimationFrame 초기화 END
 
   return (
-    <div className="p-4">
-      <div className="mb-2">
-        <h1 className="text-3xl font-bold">팀 빌딩 페이지 프로토타입</h1>
+    <div className='p-4'>
+      <div className='mb-2'>
+        <h1 className='text-3xl font-bold'>팀 빌딩 페이지 프로토타입</h1>
         <span>이게 되긴 되는건가....</span>
       </div>
 
-      <div className="mt-4 flex">
+      <div className='mt-4 flex'>
         <DragDropContext onDragEnd={handleDragDrop}>
-          <div className="grid flex-1 select-none grid-cols-2 gap-4 rounded-lg">
-            <Droppable droppableId="1" type="group">
+          <div className='grid flex-1 select-none grid-cols-2 gap-4 rounded-lg'>
+            <Droppable droppableId='1' type='group'>
               {(provided) => {
                 return (
                   <div
                     {...provided.droppableProps}
                     ref={provided.innerRef}
-                    className="bg-gray-500"
+                    className='bg-gray-500'
                   >
                     <p>1조</p>
-                    {people.map((person, idx) => (
-                      <Draggable
-                        key={person.id}
-                        draggableId={person.id}
-                        index={idx}
-                      >
-                        {(provided) => (
-                          <div
-                            {...provided.dragHandleProps}
-                            {...provided.draggableProps}
-                            ref={provided.innerRef}
-                          >
-                            {person.title}
-                          </div>
-                        )}
-                      </Draggable>
-                    ))}
+                    {people
+                      .filter((item) => item.team === '1')
+                      .map((person, idx) => (
+                        <Draggable
+                          key={person.id}
+                          draggableId={person.id}
+                          index={idx}
+                        >
+                          {(provided) => (
+                            <div
+                              {...provided.dragHandleProps}
+                              {...provided.draggableProps}
+                              ref={provided.innerRef}
+                            >
+                              {person.title}
+                            </div>
+                          )}
+                        </Draggable>
+                      ))}
                     {provided.placeholder}
                     {/* 이게 있어야 droppable 크기가 고정된다 */}
                   </div>
                 );
               }}
             </Droppable>
-            <Droppable droppableId="2" type="group">
+            <Droppable droppableId='2' type='group'>
               {(provided) => {
                 return (
                   <div
                     {...provided.droppableProps}
                     ref={provided.innerRef}
-                    className="bg-gray-500"
+                    className='bg-gray-500'
                   >
                     <p>2조</p>
+                    {people
+                      .filter((item) => item.team === '2')
+                      .map((person, idx) => (
+                        <Draggable
+                          key={person.id}
+                          draggableId={person.id}
+                          index={idx}
+                        >
+                          {(provided) => (
+                            <div
+                              {...provided.dragHandleProps}
+                              {...provided.draggableProps}
+                              ref={provided.innerRef}
+                            >
+                              {person.title}
+                            </div>
+                          )}
+                        </Draggable>
+                      ))}
+                    {provided.placeholder}
+                    {/* 이게 있어야 droppable 크기가 고정된다 */}
                   </div>
                 );
               }}
             </Droppable>
           </div>
         </DragDropContext>
+
+        <button
+          onClick={() => console.log(people.map((item) => Object.values(item)))}
+        >
+          저장
+        </button>
       </div>
     </div>
   );
