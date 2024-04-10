db5b9afb6234dec2698af86468d41b7a078e53ec
mi-hee-k <90510192+mi-hee-k@users.noreply.github.com>
2024-04-10 21:35:12 +0900

    회원가입 페이지

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,7 +1,123 @@
-import React from "react";
+'use client';
+import Image from 'next/image';
+import React, { useState } from 'react';
+import nbcIcon from '@/assets/image/scc-og.jpg';
 
 const RegisterPage = () => {
-  return <div>RegisterPage</div>;
+  const [inputs, setInputs] = useState({
+    name: '',
+    email: '',
+    password: '',
+    passwordCheck: '',
+    track: '',
+    admin: false,
+    recommend: '',
+  });
+
+  const inputHandler = (e: any) => {
+    setInputs({ ...inputs, [e.target.name]: e.target.value });
+  };
+  const inputCheckHandler = (e: any) => {
+    console.log(e);
+    setInputs({ ...inputs, [e.target.name]: e.target.checked });
+  };
+
+  const cancelRegister = () => {
+    setInputs({
+      name: '',
+      email: '',
+      password: '',
+      passwordCheck: '',
+      track: '',
+      admin: false,
+      recommend: '',
+    });
+  };
+
+  const submitHandler = (e: any) => {
+    e.preventDefault();
+    console.log(inputs);
+    alert('회원가입 성공');
+  };
+
+  return (
+    <div className='flex justify-center items-center h-screen'>
+      <div>
+        <Image src={nbcIcon} alt='이미지' width={800} height={400} />
+      </div>
+      <form
+        className='flex flex-col gap-2 justify-center items-center w-[800px]'
+        onSubmit={submitHandler}
+      >
+        <input
+          type='text'
+          name='name'
+          value={inputs.name}
+          placeholder='name'
+          className='regis-input'
+          onChange={inputHandler}
+        />
+        <input
+          type='text'
+          name='email'
+          value={inputs.email}
+          placeholder='email'
+          className='regis-input'
+          onChange={inputHandler}
+        />
+        <input
+          type='text'
+          name='password'
+          value={inputs.password}
+          placeholder='password'
+          className='regis-input'
+          onChange={inputHandler}
+        />
+        <input
+          type='text'
+          name='passwordCheck'
+          value={inputs.passwordCheck}
+          placeholder='passwordCheck'
+          className='regis-input'
+          onChange={inputHandler}
+        />
+        <select name='track' onChange={inputHandler} value={inputs.track}>
+          <option defaultValue={'트랙선택'}>트랙선택</option>
+          <option value='react3기'>리액트3기</option>
+          <option value='spring'>스프링3기</option>
+        </select>
+        <div>
+          <label htmlFor='adminCheck'>관리자이신가요?</label>
+          <input
+            type='checkbox'
+            id='adminCheck'
+            name='admin'
+            checked={inputs.admin}
+            placeholder='adminCheck'
+            onChange={inputCheckHandler}
+          />
+        </div>
+        <input
+          type='text'
+          name='recommend'
+          value={inputs.recommend}
+          placeholder='추천인 email'
+          className='regis-input'
+          onChange={inputHandler}
+        />
+        <div>
+          <button className='regis-button mr-2'>회원가입</button>
+          <button
+            type='button'
+            className='regis-button'
+            onClick={cancelRegister}
+          >
+            취소
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default RegisterPage;
