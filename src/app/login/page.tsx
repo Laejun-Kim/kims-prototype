d9af4bb2d27d3aefd5e0cdadf8f68703f087a373
Laejun Kim <49670820+Laejun-Kim@users.noreply.github.com>
2024-04-10 21:27:19 +0900

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,26 +1,62 @@
+"use client";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import nbcicon from "@/assets/image/scc-og.jpg";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const LoginPage = () => {
+  const router = useRouter();
+  const [id, setId] = useState("");
+  const [pw, setPw] = useState("");
+  const loginHandler = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("하이");
+    console.log("아이디>>", id, "비밀번호>>", pw);
+    router.push("/tempmain");
+  };
   return (
-    <>
+    <div className="이거야이거 flex h-[100vh] items-center ">
       <div className="flex items-center">
         <div>
           <Image src={nbcicon} alt="nbc icon" width={800} height={400} />
         </div>
         <div>
-          <Input type="email" placeholder="이메일을 입력하세요" />
-          <Input type="password" placeholder="비밀번호를 입력하세요" />
-          <div className="flex">
-            <Button />
-            <Button />
-          </div>
+          <form
+            onSubmit={(e) => {
+              loginHandler(e);
+            }}
+            className="flex flex-col gap-2"
+          >
+            <Input
+              type="email"
+              placeholder="이메일을 입력하세요"
+              onChange={(e) => {
+                setId(e.target.value);
+              }}
+            />
+            <Input
+              type="password"
+              placeholder="비밀번호를 입력하세요"
+              onChange={(e) => {
+                setPw(e.target.value);
+              }}
+            />
+            <div className="h-3" />
+            <div className="flex gap-2 justify-between">
+              <Button type="submit" className="bg-[#e83451]">
+                로그인
+              </Button>
+              <Link href="/register">
+                <Button>회원가입</Button>
+              </Link>
+            </div>
+          </form>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
