8b37ba14c13ee2ea5d0eb058e96f3bf5797fc9ef
Laejun Kim <49670820+Laejun-Kim@users.noreply.github.com>
2024-04-10 20:40:35 +0900

    shadcn

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,27 @@
+import Image from "next/image";
 import React from "react";
+import nbcicon from "@/assets/image/scc-og.jpg";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <>
+      <div className="flex items-center">
+        <div>
+          <Image src={nbcicon} alt="nbc icon" width={800} height={400} />
+        </div>
+        <div>
+          <Input type="email" placeholder="이메일을 입력하세요" />
+          <Input type="password" placeholder="비밀번호를 입력하세요" />
+          <div className="flex">
+            <Button />
+            <Button />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default LoginPage;
